b676cc483f2ce2ab03b8201dc8938b423dae8cfe
System Administrator <root@Khushals-MacBook-Pro.local>
2020-07-16 16:22:36 -0400

    CRUD application

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Flask_MongoDB
+
+# Running
+
+first you need to have a virtual environment installed in your PC
+
+Requirements:
+    Python3 
+    node 14.2 above
+    npm 6.17 above
+    MongoDB
+
+    To install virtal environment, follow the command below:-
+    python3 -m install --user virtualenv
+
+To run the program follow the commands below,
+
+    1)  source env/bin/activate
+    2)  export FLASK_APP=app.py
+    3)  export FLASK_DEBUG=True
+
+Now, go into the public folder type the following commands:-
+
+npm install
+npm i axios --save
+
+npm start 
+
+for more instructions, refer to README.md in public folder
--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request , jsonify
 from flask_cors import CORS
 from flask_pymongo import PyMongo
-from bson import ObjectID
-#from bson.objectid import ObjectId
-import json 
-app= Flask(__name__)
-# connect your mongodb server after install
-app.config["MONGO_URI"] = 'mongodb://localhost:27017/demo2'
+from bson import ObjectId
+import json
+
+app = Flask(__name__)
+
+app.config["MONGO_URI"]='mongodb://localhost:27017/demo2'
 mongo = PyMongo(app)
 CORS(app)
 
@@ -17,28 +17,28 @@ def getpost():
     if request.method == "GET":
         o = []
         for i in db.find():
-            o.append({"_ID":str(ObjectID(i["_id"])),"name":i["name"],"email":i["email"],"password":i["password"]})
+            o.append({"_ID":str(ObjectId(i["_id"])), "name":i["name"], "email":i["email"], "password":i["password"]})
         return jsonify(o)
-
-    elif request.method =="POST":
-        db.insert({"name":request.json["name"], "email":request.json["email"],  "password":request.json["password"],})
+    elif request.method == "POST":
+        id = db.insert({"name": request.json["name"], "email": request.json["email"], "password": request.json["password"]})
         return jsonify(str(ObjectId(id)))
-    
-@app.route('/<id>', methods=["DELETE","PUT"])
+
+
+
+@app.route('/<id>',methods=["DELETE","PUT"])
 def deleteput(id):
     if request.method == "DELETE":
         db.delete_one({"_id":ObjectId(id)})
-        return jsonify({"message": "deleted"})
-
-    elif request.method =="PUT":
-        db.update({"_id":ObjectID(id)}, {"$set":{
+        return jsonify({"message":"deleted"})
+    elif request.method == "PUT":
+        db.update({"_id":ObjectId(id)}, {"$set":{
             "name": request.json["name"],
             "email": request.json["email"],
-            "password": request.json["password"]
+            "password": request.json['password']
         }})
-        return jsonify({"message":"updated"})
-@app.route('/getone/<id>', methods=["GET"])
+        return jsonify({"message": "updated"})
+@app.route('/getone/<id>',methods=["GET"])
 def getone(id):
     res = db.find_one({"_id":ObjectId(id)})
     print(res)
-    return {"_ID":str(ObjectId("_id")), "name":res["name"], "email":res["email"], "passwword":res["password"]}
+    return {"_ID":str(ObjectId(res["_id"])), "name":res["name"],"email":res["email"],"password":res["password"] }
