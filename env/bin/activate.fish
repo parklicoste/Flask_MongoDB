8b1561b05ffb61c3829dca8aa7c7d45c9dc19c5b
parklicoste <65005378+parklicoste@users.noreply.github.com>
2020-10-04 20:17:09 -0400

    updated requirements

--- env/bin/activate.fish
+++ env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/parklicoste/Documents/Stash-projects/CRUD/env"
+set -gx VIRTUAL_ENV "/Users/parklicoste/Documents/Flask_MongoDB/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
